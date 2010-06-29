8bcdbee81a595389c19ab05b410a24cdd9b3aebc
Jeremy Hinegardner <jeremy@hinegardner.org>
2010-06-29 00:43:09 -0600

    put the stack in order

--- lib/stickler/web.rb
+++ lib/stickler/web.rb
@@ -1,5 +1,8 @@
-require 'stickler/gem_server_deflater'
-require 'stickler/gem_server'
+require 'stickler/middleware/compression'
+require 'stickler/middleware/gemcutter'
+require 'stickler/middleware/mirror'
+require 'stickler/middleware/index'
+require 'rack/cascade'
 
 module Stickler
   class Web < ::Sinatra::Base
@@ -7,9 +10,10 @@ module Stickler
     disable :sessions
     enable  :logging, :dump_errors, :clean_trace
 
-    set :root, File.expand_path( File.join( File.dirname(__FILE__), %w[ .. .. ] ) )
-
-    use Stickler::GemServerDeflater
-    use Stickler::GemServer, :repo_root => File.join( self.root, "spec", "data" )
+    use Stickler::Middleware::Compression
+    use Stickler::Middleware::Gemcutter, :serve_indexes => false
+    use Stickler::Middleware::Mirror,    :serve_indexes => false
+    use Stickler::Middleware::Index,     :serve_indexes => true
+    run Stickler::Middleware::NotFound
   end
 end
