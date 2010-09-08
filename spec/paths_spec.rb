de2fc0b33a52845758695cd4054bb28641224fad
Jeremy Hinegardner <jeremy@hinegardner.org>
2010-09-08 00:30:15 -0600

    Added paths module

---
+++ spec/paths_spec.rb
@@ -0,0 +1,13 @@
+require File.expand_path( File.join( File.dirname( __FILE__ ), "spec_helper.rb" ) )
+
+require 'stickler/paths'
+
+describe Stickler::Paths do
+  it "can access the root dir of the project" do
+    Stickler::Paths.root_dir.should == File.expand_path( File.join( File.dirname( __FILE__ ), ".." ) ) + ::File::SEPARATOR
+  end
+
+  it "can access the lib path of the project" do
+    Stickler::Paths.lib_path.should == File.expand_path( File.join( File.dirname( __FILE__ ), "..", "lib" ) ) + ::File::SEPARATOR
+  end
+end
