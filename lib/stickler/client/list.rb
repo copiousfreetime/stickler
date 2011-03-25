05f08312fb57234d87f0d6c430c0505dcafb9153
Jeremy Hinegardner <jeremy@hinegardner.org>
2011-03-24 22:43:00 -0600

    Added 'stickler list' command

--- lib/stickler/client/list.rb
+++ lib/stickler/client/list.rb
@@ -13,6 +13,17 @@ _
 
       def run
         opts = parse( self.argv )
+        repo = remote_repo_for( opts )
+        gems = Hash.new { |h,k| h[k] = Array.new }
+
+        repo.specs_list.each do |name, version, platform|
+          spec = Stickler::SpecLite.new( name, version, platform )
+          gems[name] << spec
+        end
+
+        gems.keys.sort.each do |name|
+          puts "#{name} (#{gems[name].collect { |s| s.version_platform }.join( ", " )})"
+        end
       end
    end
   end
